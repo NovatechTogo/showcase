719736909cf8ad676b468c87d4c5da721553a1ad
DATAGNI K Armand N <datagni.k.armand.n@gmail.com>
2020-10-23 13:15:18 +0000

    corrections

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
   constructor(private router: Router, public rightMenuService: RightMenuService) {
     this.router.events.subscribe((event: Event) =>{
       if(event instanceof NavigationEnd) {
-        if(this.router.url == '/' || this.router.url == '/services') {
+        if(this.router.url == '/' || this.router.url == '/services' || this.router.url == '/contact' || this.router.url == '/about') {
           document.documentElement.scrollTop = 0;
         }
       }
